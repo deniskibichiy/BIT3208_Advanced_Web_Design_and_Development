2cf5628c00c0083ce6f27270a0c825af83e62e05
Denis Kibichiy <deniskibichiy@outlook.com>
2026-06-11 15:54:08 +0300

    docs: add logbook and replace hand-drawn sketches with Draw.io mockups

--- week1/index.php
+++ week1/index.php
@@ -4,7 +4,7 @@
     <title>Week 1 - Runi Empire</title>
 </head>
 <body>
-    <h1>Runi Empire Barber Shop System</h1>
+    <h1>Student Attendance System</h1>
     <p>Week 1: Local server and PHP setup successful.</p>
 </body>
 </html>
